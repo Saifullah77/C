9430bde1f2cf98efb3fff00e4cbc1192896f7a01
Md. Saifullah <110691640+Saifullah77@users.noreply.github.com>
2022-11-09 01:25:46 +0600

    Add files via upload

---
+++ work 1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+   int T,i,n;
+if(1<=T<=10)
+if(0<=n<=5000)
+scanf("%d",&T);
+for(i=1; i<=T; i++)
+{
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("Even/n");
+}
+else 
+{
+printf("Odd/n");
+}
+}
+    return 0;
+}
+    
